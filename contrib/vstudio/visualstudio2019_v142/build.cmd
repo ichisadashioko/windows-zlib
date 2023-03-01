2895862bad2b46df5f974cbfd123cfd3e886015e
shioko <ichisadashioko@gmail.com>
2023-03-01 19:09:41 +0900

    add build config for building on Github Actions

---
+++ contrib/vstudio/visualstudio2019_v142/build.cmd
@@ -0,0 +1,4 @@
+msbuild /p:Configuration=Debug /p:Platform=x64 zlibvc.sln
+msbuild /p:Configuration=Release /p:Platform=x64 zlibvc.sln
+msbuild /p:Configuration=Debug /p:Platform=Win32 zlibvc.sln
+msbuild /p:Configuration=Release /p:Platform=Win32 zlibvc.sln
